0976c54cc842ed01a6646676c4351feca26cbd97
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2018-05-25 02:37:52 -0700

    Add back defaultValue for context

--- src/createContextOfType.jsx
+++ src/createContextOfType.jsx
@@ -1,18 +1,16 @@
 import * as React from 'react';
 
-export default function createContextOfType(type) {
+export default function createContextOfType(type, defaultValue) {
   const { Provider, Consumer } = React.createContext();
 
   class PropTypedProvider extends React.PureComponent {
-    static propTypes = {
-      value: type,
-    };
-
     render() {
       return <Provider {...this.props} />;
     }
   }
-  PropTypedProvider.displayName = 'PropTyped(Provider)'
+  PropTypedProvider.displayName = 'PropTyped(Provider)';
+  PropTypedProvider.propTypes = { value: type };
+  defaultValue && (PropTypedProvider.defaultProps = { value: defaultValue });
 
   return { Provider: PropTypedProvider, Consumer };
 }
